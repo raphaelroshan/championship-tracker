4e85b1afb4c9bd448f3c76062af57891ce1bc325
raphaelroshan <raphaelroshan@gmail.com>
2024-09-20 23:45:05 +0800

    add format guide

--- forms.py
+++ forms.py
@@ -3,12 +3,12 @@ from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 class TeamRegistrationForm(FlaskForm):
-    teams_data = TextAreaField('Enter team information', validators=[DataRequired()],
+    teams_data = TextAreaField('Enter team information with format: <Team Name> <DD/MM> <Group Number>', validators=[DataRequired()],
                                render_kw={"placeholder": "firstTeam 17/05 2\nsecondTeam 07/02 2\n\nNote: team registrations can be updated with new input"})
     submit = SubmitField('Register Teams')
 
 class MatchResultsForm(FlaskForm):
-    teams_data = TextAreaField('Enter result information', validators=[DataRequired()],
+    teams_data = TextAreaField('Enter result information with format: <Team A name> <Team B name> <Team A goals scored> <Team B goals scored>', validators=[DataRequired()],
                                render_kw={"placeholder": "firstTeam secondTeam 0 3\nthirdTeam fourthTeam 1 1"})
     submit = SubmitField('Submit Results')
 
